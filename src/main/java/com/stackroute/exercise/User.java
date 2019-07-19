873ef7f1d15e264ca4f12e33638ee93b0869cfcc
sahithi-chilakamarri <51943021+sahithi-chilakamarri@users.noreply.github.com>
2019-07-19 10:47:48 +0530

    Update User.java

--- src/main/java/com/stackroute/exercise/User.java
+++ src/main/java/com/stackroute/exercise/User.java
@@ -4,18 +4,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class User {
     private String message;
+    //Default Constructor
     public  User()
     {
     }
+    //Parameterized Constructor
     public User(String message) {
         this.message = message;
     }
+    //Getter and setter methods
     public String getMessage() {
         return message;
     }
     public void setMessage(String message) {
         this.message = message;
     }
+    //Overriding the tostring method
     @Override
     public String toString() {
         return "User{" +
