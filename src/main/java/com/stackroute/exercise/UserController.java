45d8a526358b9b28d565ac8c280931df2d271c01
sahithi-chilakamarri <51943021+sahithi-chilakamarri@users.noreply.github.com>
2019-07-19 10:49:14 +0530

    Update UserController.java

--- src/main/java/com/stackroute/exercise/UserController.java
+++ src/main/java/com/stackroute/exercise/UserController.java
@@ -6,11 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
-
+//Handler method which is used to handle all the requests ending with / 
     @RequestMapping("/")
     public String greet(Model model){
+        //Creating object for the user
         User user = new User("sahithi");
+        //Adding attribute to the view
         model.addAttribute("message",user.getMessage());
+        //Returning view
         return "index";
     }
 }
