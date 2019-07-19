4f5882fd2557de075b14958d1a39303d71ed5002
sahithi-chilakamarri <51943021+sahithi-chilakamarri@users.noreply.github.com>
2019-07-19 10:46:31 +0530

    Update FrontControllerConfig.java

--- src/main/java/com/stackroute/exercise/FrontControllerConfig.java
+++ src/main/java/com/stackroute/exercise/FrontControllerConfig.java
@@ -8,16 +8,20 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
-
+//This is the dispatcherservlet which acts as the front controller
 public class FrontControllerConfig implements WebApplicationInitializer {
 
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
+        //Creating the object
         AnnotationConfigWebApplicationContext webCtx = new AnnotationConfigWebApplicationContext();
+        //Registering the configuratrion class
         webCtx.register(Config.class);
         webCtx.setServletContext(servletContext);
+        //Adding the dispatcherservlet class object
         ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(webCtx));
         servlet.setLoadOnStartup(1);
+        //Maps to all the request which are ending with the /
         servlet.addMapping("/");
     }
 }
