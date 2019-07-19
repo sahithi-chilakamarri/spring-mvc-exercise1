35c474719d25964810f2b768ccc38578495d672e
sahithi-chilakamarri <51943021+sahithi-chilakamarri@users.noreply.github.com>
2019-07-19 10:44:14 +0530

    Update Config.java

--- src/main/java/com/stackroute/exercise/Config.java
+++ src/main/java/com/stackroute/exercise/Config.java
@@ -13,14 +13,16 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @ComponentScan(basePackages = "com.stackroute")
 
 public class Config extends WebMvcConfigurerAdapter {
-
+//This is used to create the bean for the viewresolver 
     @Bean
     public InternalResourceViewResolver internalResourceViewResolver() {
         InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        //Setting the prefix and suffix to find the view
         bean.setPrefix("/WEB-INF/");
         bean.setSuffix(".jsp");
         return bean;
     }
+    //Overriding the method in WebMvcConfigurerAdapter
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer){
         configurer.enable();
